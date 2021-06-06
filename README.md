a00b9a87cec0a5096f0b0b0f1611a7eb121f2b08
Rundi Wu <rundi_wu@pku.edu.cn>
2021-06-06 18:54:47 +0800

    update readme

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # OnShape-CAD-Parser
 
-A simple parser to collect CAD construction data from OnShape. It's built on [Onshape-public/apikey](https://github.com/onshape-public/apikey). Python 2 (2.7.9+) is required.
+A simple parser to collect CAD construction data from OnShape. It's part of our project [DeepCAD: A Deep Generative Network for Computer-Aided Design Models]().
+
+It's built on [Onshape-public/apikey](https://github.com/onshape-public/apikey). Python 2 (2.7.9+) is required.
 
 ---
 
-### Usage
+### Dependencies
 - Clone this repo
     ```sh
     $ git clone https://github.com/onshape-public/apikey.git
@@ -17,13 +19,31 @@ A simple parser to collect CAD construction data from OnShape. It's built on [On
     $ pip install -r requirements.txt
     ```
 
-- Create a `creds.json` file in the root project folder, with your Onshape developer keys: [instruction here](https://github.com/onshape-public/apikey/tree/master/python#running-the-app).
+- Follow [this instruction](https://github.com/onshape-public/apikey/tree/master/python#running-the-app) to create a `creds.json` file in the root project folder, filled with your Onshape developer keys:
+    ```json
+    {
+        "https://cad.onshape.com": {
+            "access_key": "ACCESS KEY",
+            "secret_key": "SECRET KEY"
+        }
+    }
+    ```
 
-- Run the python script
-    ```sh    
-    # some test examples
-    $ python process.py --test
-    # run on links provied by ABC dataset
-    $ python process.py
+### Usage
+- Run on some test examples:
+    ```sh
+    $ python process.py --test # some test examples
     ```
     Results are saved as JSON files following the style of [Fusion360 Gallery dataset](https://github.com/AutodeskAILab/Fusion360GalleryDataset/blob/master/docs/reconstruction.md).
+
+- ABC dataset provides a large collection of Onshape CAD designs with web links [here](https://archive.nyu.edu/handle/2451/61215). To process the downloaded links in parallel, run
+    ```sh
+    $ python process.py --link_data_dir {path of the downloaded data}
+    ```
+  We collect data for our DeepCAD paper in this way.
+
+
+### Reference
+[Placeholder]
+```
+```
--- process.py
+++ process.py
@@ -9,9 +9,6 @@ from parser import FeatureListParser
 from myclient import MyClient
 
 
-DATA_ROOT = "/home/rundi/data/abc"
-DWE_DIR = os.path.join(DATA_ROOT, 'dwe')
-
 # create instance of the OnShape client; change key to test on another stack
 c = MyClient(logging=False)
 
@@ -31,7 +28,7 @@ def process_one(data_id, link, save_dir):
             if item['message']['featureType'] not in ['newSketch', 'extrude']:
                 return 0
     except Exception as e:
-        print(data_id, "contain unsupported features:", e)
+        print("[{}], contain unsupported features:".format(data_id), e)
         return 0
 
     # parse detailed cad operations
@@ -39,7 +36,7 @@ def process_one(data_id, link, save_dir):
         parser = FeatureListParser(c, did, wid, eid, data_id=data_id)
         result = parser.parse()
     except Exception as e:
-        print(data_id, "parse failed:", e)
+        print("[{}], feature parsing fails:".format(data_id), e)
         return 0
     if len(result["sequence"]) < 2:
         return 0
@@ -50,12 +47,13 @@ def process_one(data_id, link, save_dir):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--test", action="store_true", help="test with some examples")
+parser.add_argument("--link_data_folder", default=None, type=str, help="data folder of onshape links from ABC dataset")
 args = parser.parse_args()
 
 if args.test:
-    data_examples = {# '00000352': 'https://cad.onshape.com/documents/4185972a944744d8a7a0f2b4/w/d82d7eef8edf4342b7e49732/e/b6d6b562e8b64e7ea50d8325',
+    data_examples = {'00000352': 'https://cad.onshape.com/documents/4185972a944744d8a7a0f2b4/w/d82d7eef8edf4342b7e49732/e/b6d6b562e8b64e7ea50d8325',
                      '00001272': 'https://cad.onshape.com/documents/b53ece83d8964b44bbf1f8ed/w/6b2f1aad3c43402c82009c85/e/91cb13b68f164c2eba845ce6',
-                     # '00001616': 'https://cad.onshape.com/documents/8c3b97c1382c43bab3eb1b48/w/43439c4e192347ecbf818421/e/63b575e3ac654545b571eee6',
+                     '00001616': 'https://cad.onshape.com/documents/8c3b97c1382c43bab3eb1b48/w/43439c4e192347ecbf818421/e/63b575e3ac654545b571eee6',
                     }
     save_dir = "examples"
     if not os.path.exists(save_dir):
@@ -65,10 +63,13 @@ if args.test:
         process_one(data_id, link, save_dir)
 
 else:
+    DWE_DIR = args.link_data_folder
+    DATA_ROOT = os.path.dirname(DWE_DIR)
     filenames = sorted(os.listdir(DWE_DIR))
     for name in filenames:
         truck_id = name.split('.')[0].split('_')[-1]
         print("Processing truck: {}".format(truck_id))
+
         save_dir = os.path.join(DATA_ROOT, "processed/{}".format(truck_id))
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
